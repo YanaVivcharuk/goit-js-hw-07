ead4abe95e2e1336a267f36deef63502e0d2d9df
YanaVivcharuk <yanavivcharuk@icloud.com>
2025-05-20 22:10:41 +0200

    js7

---
+++ js/task-3.js
@@ -0,0 +1,7 @@
+const inputEl = document.querySelector("#name-input");
+const outputEl = document.querySelector("#name-output");
+
+inputEl.addEventListener("input", (event) => {
+  const trimmedValue = event.target.value.triam();
+  outputEl.textContent = trimmedValue || "Anonymous";
+});
